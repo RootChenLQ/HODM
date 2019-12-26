fbf82782c414ed0c119b82b9f089fdfe661106b8
784741054@qq.com <784741054@qq.com>
2019-12-26 15:27:34 +0800

    191226

--- KitNET/Myexample.py
+++ KitNET/Myexample.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 import time
 import matplotlib.pyplot as plt
+import InsertNoise
 ##############################################################################
 # KitNET is a lightweight online anomaly detection algorithm based on an ensemble of autoencoders.
 # For more information and citation, please see our NDSS'18 paper: Kitsune: An Ensemble of Autoencoders for Online Network Intrusion Detection
@@ -24,17 +25,22 @@ with zipfile.ZipFile("dataset.zip","r") as zip_ref:
 
 print("Reading Sample dataset...")
 '''
-X = pd.read_csv("node43.csv",header=None) #an m-by-n dataset with m observations
-X = X[0:20000].copy()
 # KitNET params:
 maxAE = 10  #maximum size for any autoencoder in the ensemble layer
-#FMgrace = 5000 #the number of instances taken to learn the feature mapping (the ensemble's architecture)
-ADgrace = 5000 #the number of instances used to train the anomaly detector (ensemble itself)
+FMgrace = 0 #the number of instances taken to learn the feature mapping (the ensemble's architecture)
+ADgrace = 4000 #the number of instances used to train the anomaly detector (ensemble itself)
+
+# load data
+X = pd.read_csv("KitNET/node43.csv",header=None) #an m-by-n dataset with m observations
+X = X[0:10000].copy()
+X,outlier_pos1  = InsertNoise.insert_noise_error(X,[0],ADgrace,1000,0.5,1.5)
+
+
 
 # Build KitNET
 #(self,n,max_autoencoder_size=10,FM_grace_period=None,AD_grace_period=10000,learning_rate=0.1,hidden_ratio=0.75, feature_map = None):
-K = kit.KitNET(X.shape[1],max_autoencoder_size=10,FM_grace_period=None,
-                    AD_grace_period=5000,learning_rate=0.1,hidden_ratio=2/3, feature_map = None)
+K = kit.KitNET(X.shape[1],max_autoencoder_size= 10,FM_grace_period=FMgrace,
+                    AD_grace_period=ADgrace,learning_rate=0.1,hidden_ratio=2/3, feature_map = None)
 RMSEs = np.zeros(X.shape[0]) # a place to save the scores
 
 print("Running KitNET:")
@@ -47,21 +53,26 @@ for i in range(X.shape[0]):
     RMSEs[i] = K.process(X.loc[i]) #will train during the grace periods, then execute on all the rest.
 stop = time.time()
 print("Complete. Time elapsed: "+ str(stop - start))
-x = [i for i in range(len(RMSEs))]
-plt.plot(x,RMSEs)
+x = [i for i in range(len(RMSEs)-ADgrace)]
+plt.plot(x,RMSEs[ADgrace:len(RMSEs)])
 plt.show()
 
 # Here we demonstrate how one can fit the RMSE scores to a log-normal distribution (useful for finding/setting a cutoff threshold \phi)
+
 from scipy.stats import norm
-benignSample = np.log(RMSEs[ADgrace+1:X.shape[0]])
-logProbs = norm.logsf(np.log(RMSEs), np.mean(benignSample), np.std(benignSample))
-'''
+beginSample = np.log(RMSEs[ADgrace:len(RMSEs)])
+logProbs = norm.logsf(np.log(RMSEs), np.mean(beginSample), np.std(beginSample))
+
+fig = plt.scatter(x,RMSEs[FMgrace+ADgrace:],s=0.1,c=logProbs[FMgrace+ADgrace+1:],cmap='RdYlGn')
+
+
 # plot the RMSE anomaly scores
+'''
 print("Plotting results")
 from matplotlib import pyplot as plt
 from matplotlib import cm
 plt.figure(figsize=(10,5))
-timestamps = pd.read_csv("mirai3_ts.csv",header=None).as_matrix()
+timestamps = pd.read_csv("KitNET/mirai3_ts.csv",header=None).as_matrix()
 fig = plt.scatter(timestamps[FMgrace+ADgrace+1:],RMSEs[FMgrace+ADgrace+1:],s=0.1,c=logProbs[FMgrace+ADgrace+1:],cmap='RdYlGn')
 plt.yscale("log")
 plt.title("Anomaly Scores from KitNET's Execution Phase")
@@ -71,5 +82,4 @@ plt.annotate('Mirai C&C channel opened [Telnet]', xy=(timestamps[71662],RMSEs[71
 plt.annotate('Mirai Bot Activated\nMirai scans network for vulnerable devices', xy=(timestamps[72662],1), xytext=(timestamps[55000],5),arrowprops=dict(facecolor='black', shrink=0.05),)
 figbar=plt.colorbar()
 figbar.ax.set_ylabel('Log Probability\n ', rotation=270)
-plt.show()
-'''
+plt.show()'''
